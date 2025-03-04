32b25ce4d1654893fa108c00d1440e834fab3461
rifathassan478 <rifathassanrana@gmail.com>
2025-03-05 00:40:38 +0600

    Added style.css file, hero and category section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            'primary-color': 'rgb(255, 220, 38)',
+            'nav-bg': 'rgb(255, 249, 244)',
+            'card-bg': 'rgb(249, 223, 138)',
+        },
+        fontFamily: {
+            'main-font': 'Sora',
+            'sub-font': 'ADLaM Display',
+        }
+      },
     },
     plugins: [],
 }
