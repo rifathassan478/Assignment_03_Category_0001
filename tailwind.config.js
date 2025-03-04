d0b31dd695e35df389235543a01ac9c784741522
rifathassan478 <rifathassanrana@gmail.com>
2025-03-05 00:46:10 +0600

    Added product and price section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
             'primary-color': 'rgb(255, 220, 38)',
             'nav-bg': 'rgb(255, 249, 244)',
             'card-bg': 'rgb(249, 223, 138)',
+            'card-color-1': 'rgb(248, 204, 251)',
+            'card-color-2': 'rgb(172, 210, 253)',
+            'card-color-3': 'rgb(255, 249, 244)',
+            'card-color-4': 'rgb(249, 223, 138)',
         },
         fontFamily: {
             'main-font': 'Sora',
